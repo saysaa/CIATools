04acea5b53b45fe742c04104767927d2eca19136
Saiitanaa <112180175+saiitanaa@users.noreply.github.com>
2026-09-06 17:22:35 +0200

    Delete RSSCRIPT directory

--- RSSCRIPT/runrusttime/install_windows.ps1
+++
@@ -1,5 +0,0 @@
-cargo build --release --bins
-Copy-Item target\release\import.exe ..\import.exe -Force
-Copy-Item target\release\compile.exe ..\compile.exe -Force
-Copy-Item target\release\delete.exe ..\delete.exe -Force
-Write-Host "Windows executables installed in RSSCRIPT/"
